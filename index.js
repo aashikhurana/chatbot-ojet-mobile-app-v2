70fa75dcacd1196c33734631851a0c5bd967b271
aaskhura <aaskhura@AASKHURA-IN.in.oracle.com>
2018-03-13 15:06:01 +0530

    app_abc_4

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var express = require('express');
+var os = require("os");
 var app = express();
 console.log("express created");
 app.use(express.static(path.join(__dirname, 'web')));
@@ -11,9 +12,9 @@ app.get('/', function(req, res){
 });
 
 var port = process.env.PORT || 3000; 
-
+var host = os.hostname();
 console.log('Listening on port ' +port);
 
 app.listen(port);
 
-console.log('Listening on port ' +port);
+console.log('Listening at host ' +host);
