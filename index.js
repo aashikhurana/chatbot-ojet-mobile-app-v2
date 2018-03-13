980bde5b7c8c68628646972b7d6c15b5b89c6e6b
aaskhura <aaskhura@AASKHURA-IN.in.oracle.com>
2018-03-13 12:58:35 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var path = require('path');
+var express = require('express');
+var app = express();
+console.log("express created");
+app.use(express.static(path.join(__dirname, 'web')));
+console.log("getting index.html");
+
+app.get('/', function(req, res){
+	console.log("rendering index.html");
+    res.render('index.html');
+});
+
+app.listen(process.env.PORT || 3000);
+console.log('Server running on port 3000');
